5b68612dd82e960410789bae3477126e239e055b
Daniel Saraiva <saraivadaniel@protonmail.com>
2025-03-12 11:00:22 -0300

    Rota para cadastrar criada

--- backend/crud_produtos/src/main/java/br/com/anm/produtos/crud_produtos/servico/ProdutoServico.java
+++ backend/crud_produtos/src/main/java/br/com/anm/produtos/crud_produtos/servico/ProdutoServico.java
@@ -1,9 +1,12 @@
 package br.com.anm.produtos.crud_produtos.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.anm.produtos.crud_produtos.modelo.ProdutoModelo;
+import br.com.anm.produtos.crud_produtos.modelo.RespostaModelo;
 import br.com.anm.produtos.crud_produtos.repositorio.ProdutoRepositorio;
 
 @Service
@@ -12,7 +15,28 @@ public class ProdutoServico {
     @Autowired
     private ProdutoRepositorio pr;
 
+    @Autowired
+    private RespostaModelo rm;
+
+    //Listar produtos
     public Iterable<ProdutoModelo> listar(){
         return pr.findAll();
     }
+
+    //Cadastrar produtos
+    public ResponseEntity<?> cadastrar(ProdutoModelo pm){
+        if(pm.getNome().equals("")){
+            rm.setResposta("O nome do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }else if(pm.getMarca().equals("")){
+            rm.setResposta("O nome da marca do produto é obrigratório!");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }else{
+            return new ResponseEntity<ProdutoModelo>(pr.save(pm),HttpStatus.CREATED);
+        }
+
+
+    }
+
+
 }
