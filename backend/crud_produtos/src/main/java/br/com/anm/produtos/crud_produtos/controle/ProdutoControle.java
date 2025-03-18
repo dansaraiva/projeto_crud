3a2046120e5fdbbbcb9707e1924eb6f66a4aa099
Daniel Saraiva <saraivadaniel@protonmail.com>
2025-03-18 11:33:29 -0300

    Rota para remover criada

--- backend/crud_produtos/src/main/java/br/com/anm/produtos/crud_produtos/controle/ProdutoControle.java
+++ backend/crud_produtos/src/main/java/br/com/anm/produtos/crud_produtos/controle/ProdutoControle.java
@@ -2,22 +2,30 @@ package br.com.anm.produtos.crud_produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.anm.produtos.crud_produtos.modelo.ProdutoModelo;
+import br.com.anm.produtos.crud_produtos.modelo.RespostaModelo;
 import br.com.anm.produtos.crud_produtos.servico.ProdutoServico;
 
 @RestController
 public class ProdutoControle {
     //Injeção de dependências
     @Autowired
-    private ProdutoServico ps;    
+    private ProdutoServico ps;  
+    
+    @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo){
+        return ps.remover(codigo);
+    }
 
-     @PostMapping("/cadastrar")
+    @PostMapping("/cadastrar")
     public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
         return ps.cadastrarAlterar(pm, "cadastrar");
     }
