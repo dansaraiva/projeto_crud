166def10f258b9fb4f4cd0bcce5674ef84e72ef2
Daniel Saraiva <saraivadaniel@protonmail.com>
2025-04-01 13:19:46 -0300

    Bootstrap instalado, componentes de formulário e tabela criados

---
+++ frontend/front/src/Formulario.jsx
@@ -0,0 +1,17 @@
+function Formulario(){
+    return(
+        <form>
+            <h1>Formulario</h1>
+            <input type="text" placeholder="Nome"></input>
+            <input type="text" placeholder="Marca"></input>
+
+            <input type="button" value="Cadastrar" />
+            <input type="button" value="Cancelar" />
+            <input type="button" value="Alterar" />
+            <input type="button" value="Remover" />
+
+        </form>
+    )
+}
+
+export default Formulario;
