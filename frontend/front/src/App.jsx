563b5cfbeec9f0a6b5eb0cc59f9610efc2583227
Daniel Saraiva <saraivadaniel@protonmail.com>
2025-04-29 08:01:55 -0300

    Front conectado ao back

--- frontend/front/src/App.jsx
+++ frontend/front/src/App.jsx
@@ -1,15 +1,26 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
 
-const[btnCadastrar] =useState(false);
+const[btnCadastrar] =useState(true);
+const[produtos, setProdutos] =useState([]);
+
+
+  //UseEffect
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
+
 
 return (    
       <div>
+        <p>{JSON.stringify(produtos)}</p>
         <Formulario botao={btnCadastrar}/>
         <Tabela/>
       </div>
